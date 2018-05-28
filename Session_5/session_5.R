2f6b3399f5af03427686301095b4b25cbec9e119
Philipp Behrendt <philipp.behrendt@uni-konstanz.de>
2018-05-28 13:33:49 +0200

    delete draft session5

--- Session_5/session_5.R
+++
@@ -1,7 +0,0 @@
-##########################################################################
-#
-# Introduction to Data Collection with R
-# Session 5: Visualisierungen
-# Philipp Behrendt (philipp.behrendt@uni.kn) & Jan Dix (jan.dix@uni.kn)
-#
-##########################################################################
