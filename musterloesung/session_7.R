2db06a8c3098c4f31df1255b133f926c0e8ed8bd
jandix <dix.jan@gmx.de>
2018-06-12 15:03:12 +0200

    update sample

--- musterloesung/session_7.R
+++ musterloesung/session_7.R
@@ -13,7 +13,7 @@ gtdb <- readRDS("data/gtdb_2012_cc.rds")
 #########################################################################
 # TODO: Erstelle ein Sample basierend auf einem Land (z.B.: Kolumbien).
 #########################################################################
-gtdb_country_sample <- gtdb[gtdb$country_code == "us", ]
+gtdb_country_sample <- 
   
 # date erstellen
 gtdb_country_sample$date <- make_date(year = gtdb_country_sample$iyear,
@@ -22,20 +22,28 @@ gtdb_country_sample$date <- make_date(year = gtdb_country_sample$iyear,
 
 # parameter
 zeit_spanne <- 5 # 5 Tage
-api_key <- "eab334ada11a40fa946dce12fa551859"
+api_key <- "xxx"
 
 for (i in 1:nrow(gtdb_country_sample)) {
   
   # begin date
-  begin <- gsub("-", "", gtdb$date[i])
+  begin <- gsub("-", "", gtdb_country_sample$date[i])
   
   # end date 
-  end <- gsub("-", "", gtdb$date[i] + ddays(zeit_spanne))
+  end <- gsub("-", "", gtdb_country_sample$date[i] + ddays(zeit_spanne))
   
   #########################################################################
-  # TODO: Query definieren.
+  # TODO: Query definieren. (Tipp: paste())
   #########################################################################
-  query <- gtdb_country_sample$targtype1_txt[i]
+  query <- 
+  
+  if (!is.na(gtdb_country_sample$province[i])) {
+    query <- 
+  }
+  
+  if (!is.na(gtdb_country_sample$city[i])) {
+    query <- 
+  }
   
   # get articles
   result <- as_search(q = query,
@@ -44,23 +52,25 @@ for (i in 1:nrow(gtdb_country_sample)) {
                       all_results = T,
                       key = api_key)
   
-  if (!exists("articles")) {
-    articles <- data.frame(nyt_id = result$data$`_id`,
-                           headline = result$data$headline.main,
-                           url = result$data$web_url,
-                           pub_date = result$data$pub_date,
-                           word_count = result$data$word_count,
-                           event_id = gtdb_country_sample$eventid[i],
-                           stringsAsFactors = F)
-  } else {
-    article <- data.frame(nyt_id = result$data$`_id`,
-                          headline = result$data$headline.main,
-                          url = result$data$web_url,
-                          pub_date = result$data$pub_date,
-                          word_count = result$data$word_count,
-                          event_id = gtdb_country_sample$eventid[i],
-                          stringsAsFactors = F)
-    articles <- rbind(articles, article)
+  if (nrow(result$data) > 0) {
+    if (!exists("articles")) {
+      articles <- data.frame(nyt_id = result$data$`_id`,
+                             headline = result$data$headline.main,
+                             url = result$data$web_url,
+                             pub_date = result$data$pub_date,
+                             word_count = result$data$word_count,
+                             event_id = gtdb_country_sample$eventid[i],
+                             stringsAsFactors = F)
+    } else {
+      article <- data.frame(nyt_id = result$data$`_id`,
+                            headline = result$data$headline.main,
+                            url = result$data$web_url,
+                            pub_date = result$data$pub_date,
+                            word_count = result$data$word_count,
+                            event_id = gtdb_country_sample$eventid[i],
+                            stringsAsFactors = F)
+      articles <- rbind(articles, article)
+    }
   }
   
   Sys.sleep(1)
